54fb72a9dd3e52f09ac6fcc4f90a6058b3fcbd9b
Mojtaba Abedi <68202630+s-mojtaba-a@users.noreply.github.com>
2020-09-18 00:18:29 +0430

    Update Cpp.cpp

--- Cpp.cpp
+++ Cpp.cpp
@@ -63,3 +63,45 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 }
+
+
+#### python impelemenation ### 
+def lps(s):
+    i = 0
+    j = 0
+    m = len(s)
+    arr = [0]*m
+    while j < m:
+        if s[i] == s[j]:
+            arr[j] = i+1
+            i += 1
+            j += 1
+        else:
+            if i == 0:
+                arr[j] = 0
+                j += 1
+            else:
+                i = arr[i-1]
+    return(arr)
+
+
+def kmp(s1, s2, arr):
+    ans = []
+    n = len(s1)
+    m = len(s2)
+    i = 0
+    j = 0
+    while j < n:
+        if (s1[j] == s2[i]):
+            i += 1
+            j += 1
+        else:
+            if i != 0:
+                i = arr[i-1]
+            else:
+                j += 1
+        if i == m:
+            i = arr[i-1]
+            ans.append(j-m)
+    return ans
+
